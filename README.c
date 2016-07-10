eeb1c3f78da0f9ac9ba7fa5d0643ba4e6f0f2304
mahendiran43 <kmahee000@gmail.com>
2016-07-10 14:48:04 +0530

    finish

---
+++ README.c
@@ -0,0 +1,39 @@
+# de
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,count=0,i=0,sum=0,r[10];
+
+clrscr();
+
+printf("enter the number of children");
+
+scanf("%d",&n);
+
+printf("enter the rating of the each children");
+
+for(i=0;i<n;i++)
+
+{
+
+scanf("%d",&r[i]);
+
+}
+{
+for(i=0;i<n;i++)
+{
+if(r[i]<r[i+1])
+{
+count=count+1;
+sum=sum+count;
+}
+else
+sum=sum+count;
+}
+}
+printf("minimum candies%d",sum);
+
+getch();
+
+}
